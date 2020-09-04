ef5a61e3b15cc1576250aa905d30ebc4b0e1f5fe
yuying <7884765+oui_zss@user.noreply.gitee.com>
2020-09-04 17:33:28 +0800

    第三次提交

---
+++ src/test3/Test3.java
@@ -0,0 +1,13 @@
+package test3;
+
+public class Test3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("H3");
+	}
+
+}
